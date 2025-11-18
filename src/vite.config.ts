23ba107715f32dbb2df74daf805bedc5218abe5f
Adityadn64 <adityadwinugraha.2021@gmail.com>
2025-11-19 06:47:32 +0700

    REACT #10

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import fs from 'fs';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig(({ mode }) => {
@@ -18,7 +19,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [
         react(),
-        basicSsl()
+        basicSsl(),
+        tailwindcss(),
       ],
       define: {
         'process.env.DEFAULT_SERVER_API_URL': JSON.stringify(env.DEFAULT_SERVER_API_URL),
